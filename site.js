457087038726b896df9d76c60df111ff9024d799
Sofia Batigniani <lojainformatica04@gmail.com>
2023-05-15 10:46:23 -0300

    Adicionando o slide em HTML CSS E JAVASCRIPT

---
+++ site.js
@@ -0,0 +1,48 @@
+let totalSlides = document.querySelectorAll('.slider--item').length;
+let currentSlide = 0;
+
+let sliderWidth = document.querySelector('.slider').clientWidth;
+
+document.querySelector('.slider--width').style.width = 
+    `${sliderWidth * totalSlides}px`;
+
+document.querySelector('.slider--controls').style.width = 
+    `${sliderWidth}px`;
+document.querySelector('.slider--controls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+}
+function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalSlides-1)) {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin() {
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = 
+        `-${newMargin}px`;
+}
+
+setInterval(goNext, 5000);
+function toggleMenu() {
+
+	var menu = document.getElementById("menu");
+
+	if (menu.style.display == 'none' || menu.style.display == '') {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none";
+	}
+
+
+}
